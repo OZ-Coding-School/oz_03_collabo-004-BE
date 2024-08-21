528a8f9e7ec0791e02cc81d2f2277f08f46fc4ae
dayeonkimm <ekdyd516@gmail.com>
2024-08-21 14:11:50 +0900

    Feat: article, comment 모델 생성

--- api/comments/models.py
+++ api/comments/models.py
@@ -1,3 +1,20 @@
+from articles.models import Article
+from common.models import TimeStampModel
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Comment(TimeStampModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    articles = models.ForeignKey(Article, on_delete=models.CASCADE, null=False)
+    content = models.TextField(null=False)
+    is_selected = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ("user", "article")
+
+
+class CommentImage(TimeStampModel):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=False)
+    image_url = models.CharField(max_length=255, null=False)
+    is_thumbnail = models.BooleanField(default=False)
