9ea64991cbe11b0450b8e3c596c2f902616a93ed
yoonju977 <yoonju977@gmail.com>
2024-08-21 15:00:34 +0900

    배포테스트환경 세팅_수정

--- api/config/settings.py
+++ api/config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
 DEBUG = True
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS").split(",")
