44cf5b3d9bd2837a7547486b63c9eacc6fb33e5c
yoonju977 <yoonju977@gmail.com>
2024-08-24 08:29:43 +0900

    comment image_null=true

--- api/comments/models.py
+++ api/comments/models.py
@@ -3,9 +3,12 @@ from common.models import TimeStampModel
 from django.db import models
 from users.models import User
 
+
 class Comment(TimeStampModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=False, related_name='comments')
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, null=False, related_name="comments"
+    )
     content = models.TextField(null=False)
     is_selected = models.BooleanField(default=False)
     helpful_count = models.IntegerField(default=0)  # 도움이 돼요 수
@@ -17,21 +20,27 @@ class Comment(TimeStampModel):
     def __str__(self):
         return f"Comment by {self.user.username} on {self.article.title}"
 
+
 class CommentImage(TimeStampModel):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=False, related_name='images')
-    image = models.ImageField(upload_to="comments/", null=False)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, null=False, related_name="images"
+    )
+    image = models.ImageField(upload_to="comments/", null=True, blank=True)
 
     def __str__(self):
         return f"{self.comment.id} - {self.id} - {self.image.name}"
 
+
 class CommentReaction(TimeStampModel):
     REACTION_CHOICES = (
-        ('helpful', 'Helpful'),
-        ('not_helpful', 'Not Helpful'),
+        ("helpful", "Helpful"),
+        ("not_helpful", "Not Helpful"),
     )
-    
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=False, related_name='reactions')
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, null=False, related_name="reactions"
+    )
     reaction_type = models.CharField(max_length=12, choices=REACTION_CHOICES)
 
     class Meta:
