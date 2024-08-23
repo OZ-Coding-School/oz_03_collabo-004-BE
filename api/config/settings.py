9c447e0cc0223cf0af7081255dbed040849e9bce
dayeonkimm <ekdyd516@gmail.com>
2024-08-23 16:06:30 +0900

    refactor: middleware 추가

--- api/config/settings.py
+++ api/config/settings.py
@@ -108,6 +108,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
