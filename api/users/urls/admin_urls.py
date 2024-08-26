58d7226cac13b1cc7fe7934a8a008844f4a28a45
yoonju977 <yoonju977@gmail.com>
2024-08-26 18:19:36 +0900

    admin_crud

--- api/users/urls/admin_urls.py
+++ api/users/urls/admin_urls.py
@@ -13,31 +13,25 @@ from api.users.views.user_crud_views import (
 urlpatterns = [
     # 유저 권한 변경
     path(
-        "users/<int:id>/change-role/",
+        "state/<int:id>/",
         SwitchUserAuthorizationView.as_view(),
         name="change-user-role",
     ),
-    # 모든 유저 정보 조회
     path("users/", UserListForAdmin.as_view(), name="user-list"),
-    # 특정 유저 정보 조회, 수정, 삭제
     path(
-        "users/<int:id>/",
+        "user/<int:id>/",
         UserRetrieveUpdateDeleteAdminView.as_view(),
         name="user-detail",
     ),
-    # 특정 게시글 조회 및 삭제
-    path("articles/<int:id>/", AdminArticleDetailView.as_view(), name="article-detail"),
-    # 특정 댓글 조회 및 삭제
-    path("comments/<int:id>/", AdminCommentDetailView.as_view(), name="comment-detail"),
-    # 특정 사용자가 작성한 게시글과 댓글 조회
+    path("article/<int:id>/", AdminArticleDetailView.as_view(), name="article-detail"),
+    path("comment/<int:id>/", AdminCommentDetailView.as_view(), name="comment-detail"),
     path(
-        "users/<int:id>/articles-comments/",
+        "user/<int:id>/articles-comments/",
         UserArticlesCommentsView.as_view(),
         name="user-articles-comments",
     ),
-    # 특정 사용자가 작성한 게시글과 댓글 삭제
     path(
-        "users/<int:id>/delete-articles-comments/",
+        "user/<int:id>/articles-comments/delete/",
         UserArticleCommentDeleteView.as_view(),
         name="user-delete-articles-comments",
     ),
