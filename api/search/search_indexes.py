e34664e9748805018e918f5324e6d21ad84636f1
yoonju977 <yoonju977@gmail.com>
2024-09-01 20:55:37 +0900

    검색 결과 수정

--- api/search/search_indexes.py
+++ api/search/search_indexes.py
@@ -2,15 +2,18 @@ from articles.models import Article
 from elasticsearch_dsl import Date, Document, Index, Integer, Text, analyzer, tokenizer
 from elasticsearch_dsl.connections import connections
 
+# Elasticsearch 연결 설정
 connections.create_connection(hosts=["http://elasticsearch:9200"])
 
+# Korean Analyzer 정의
 korean_analyzer = analyzer(
     "korean_analyzer",
-    tokenizer=tokenizer("nori_tokenizer"),
+    tokenizer="nori_tokenizer",
     filter=["lowercase", "nori_part_of_speech", "nori_readingform"],
 )
 
 
+# 인덱스 정의 및 설정
 class ArticleDocument(Document):
     title = Text(analyzer=korean_analyzer)
     content = Text(analyzer=korean_analyzer)
@@ -29,8 +32,25 @@ class ArticleDocument(Document):
         )
 
 
-# Create the index with the custom settings
+# 인덱스 생성 또는 업데이트
 index = Index(ArticleDocument.Index.name)
-index.settings(number_of_shards=1, number_of_replicas=0)
-index.analyzer(korean_analyzer)
-index.create()
+index.settings(
+    number_of_shards=1,
+    number_of_replicas=0,
+    analysis={
+        "analyzer": {
+            "korean_analyzer": {
+                "type": "custom",
+                "tokenizer": "nori_tokenizer",
+                "filter": ["lowercase", "nori_part_of_speech", "nori_readingform"],
+            }
+        }
+    },
+)
+
+# 매핑을 인덱스에 추가
+index.document(ArticleDocument)
+
+# 인덱스가 이미 존재하지 않는 경우에만 생성
+if not index.exists():
+    index.create()
