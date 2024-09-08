64feab85b8823eab28cd55525f79a64fac7cf351
yoonju977 <yoonju977@gmail.com>
2024-09-09 05:30:23 +0900

    locust_final

--- api/users/utils.py
+++ api/users/utils.py
@@ -1,15 +1,16 @@
 import os
 from dataclasses import dataclass, field
-
+from pathlib import Path
 
 import pytz
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils import timezone
-
+from dotenv import load_dotenv
 from rest_framework.permissions import BasePermission
 from rest_framework_simplejwt.tokens import RefreshToken
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 @dataclass
@@ -108,9 +109,10 @@ class HunsooKingAuthClass:
             key=key,
             value=token,
             httponly=True,
-            samesite="Lax",
-            secure=False,
+            samesite="None",
+            secure=True,
             expires=expires_at,
+            domain=os.getenv("COOKIE_DOMAIN"),
             path="/",
         )
 
@@ -201,14 +203,14 @@ class GeneralAuthClass:
         else:
             raise ValueError("key should be 'access' or 'refresh'")
 
-
         response.set_cookie(
             key=key,
             value=token,
             httponly=True,
-            samesite="Lax",
-            secure=False,
+            samesite="None",
+            secure=True,
             expires=expires_at,
+            domain=os.getenv("COOKIE_DOMAIN"),
             path="/",
         )
         return response
