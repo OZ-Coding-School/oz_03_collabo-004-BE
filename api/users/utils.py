e2eff2363f4f55f6d58c8b4a4439881083ee282c
yoonju977 <yoonju977@gmail.com>
2024-09-11 02:28:39 +0900

    drop_locust,jenkis

--- api/users/utils.py
+++ api/users/utils.py
@@ -1,16 +1,16 @@
 import os
 from dataclasses import dataclass, field
-#from pathlib import Path
+from pathlib import Path
 
 import pytz
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils import timezone
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from rest_framework.permissions import BasePermission
 from rest_framework_simplejwt.tokens import RefreshToken
 
-#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 @dataclass
@@ -109,9 +109,10 @@ class HunsooKingAuthClass:
             key=key,
             value=token,
             httponly=True,
-            samesite="Lax",
-            secure=False,
+            samesite="None",
+            secure=True,
             expires=expires_at,
+            domain=os.getenv("COOKIE_DOMAIN"),
             path="/",
         )
 
@@ -206,11 +207,13 @@ class GeneralAuthClass:
             key=key,
             value=token,
             httponly=True,
-            samesite="Lax",
-            secure=False,
+            samesite="None",
+            secure=True,
             expires=expires_at,
+            domain=os.getenv("COOKIE_DOMAIN"),
             path="/",
         )
+        
         return response
 
     @staticmethod
