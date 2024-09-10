b83c201264f6c73dfdf946be2c28258285805d4e
yoonju977 <yoonju977@gmail.com>
2024-09-10 19:39:19 +0900

    like_list

--- api/articles/views/article_list_views.py
+++ api/articles/views/article_list_views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 from ..models import Article
 from ..serializers import (
@@ -32,3 +33,25 @@ class ArticleByTagView(generics.ListAPIView):
     def get_queryset(self):
         tag_id = self.kwargs["tag_id"]
         return Article.objects.filter(tags__tag_id=tag_id).order_by("-created_at")
+
+
+# 전체 게시글 조회 리스트 (like 상태와 article_id만 반환)
+class ArticleLikeListView(generics.ListAPIView):
+    queryset = Article.objects.all().order_by("-created_at")
+    serializer_class = ArticleListSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(
+            queryset, many=True, context={"request": request}
+        )
+
+        # 필요한 응답 형식으로 변환
+        response_data = []
+        for article_data in serializer.data:
+            response_data.append(
+                {"like": article_data["like"], "article_id": article_data["article_id"]}
+            )
+
+        return Response(response_data)
